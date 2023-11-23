c40912fe04420f89e8a5dad9b940d8bb8dc87919
Binhao <146821720+PaxuitoGIT@users.noreply.github.com>
2023-11-23 11:42:11 +0000

    boost

---
+++ boost.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <boost/lexical_cast.hpp>
+
+int main() {
+    string str = "123";
+
+    try {
+        int num = boost::lexical_cast<int>(str);
+        cout << "El numero es: " << num << endl;
+    } catch(boost::bad_lexical_cast &e) {
+        cout << "Error: " << e.what() << endl;
+    }
+
+    return 0;
+}
